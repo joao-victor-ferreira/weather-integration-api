df45fd3f12d60533809dcf371a399daa343dd2a6
joao-victor-ferreira <jsousaferreira101@icloud.com>
2026-08-29 16:29:49 -0300

    feat: add health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import configuration from "./config/configuration.js";
 import { PrismaModule } from "./database/prisma/prisma.module.js";
-import { WeatherModule } from "./modules/weather/weather.module.js";
 import { HealthModule } from "./health/health.module.js";
+import { WeatherModule } from "./modules/weather/weather.module.js";
 
 @Module({
 	imports: [
@@ -14,7 +14,6 @@ import { HealthModule } from "./health/health.module.js";
 		HealthModule,
 		PrismaModule,
 		WeatherModule,
-		
 	],
 })
 export class AppModule {}
